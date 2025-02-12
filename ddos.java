8b2ecbcd335aa8d8963466cf07d784e7a855203c
KomaVR <komavr12454@gmail.com>
2025-02-12 13:18:39 -0800

    Update ddos.java

--- ddos.java
+++ ddos.java
@@ -19,7 +19,7 @@ public class DdosAttack {
     public static class DdosThread extends Thread {
 
         private AtomicBoolean running = new AtomicBoolean(true);
-        private final String request = "https://www.hackthissite.org";
+        private final String request = "https://www.blobtown.com";
         private final URL url;
 
         String param = null;
